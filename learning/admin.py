5caa5e3ae62917253525afc8b63dc8016514c61d
Chen-yuping <2062741277@qq.com>
2026-01-14 18:08:20 +0800

    Delete learning directory

--- learning/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import ExerciseFile
-
-@admin.register(ExerciseFile)
-class ExerciseFileAdmin(admin.ModelAdmin):
-    list_display = ['original_filename', 'teacher', 'subject', 'status', 'uploaded_at', 'exercise_count']
-    list_filter = ['status', 'subject', 'uploaded_at']
-    search_fields = ['original_filename', 'teacher__username']
-
